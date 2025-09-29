4b7a627a8c5a36afd6c8593a9650e9d0e3e9e5fd
Maxim <furrytales@yandex.ru>
2025-09-29 23:35:50 +0500

    levels perchat, improved ai modules

--- _/ai-talker.js
+++ _/ai-talker.js
@@ -8,44 +8,29 @@ class AITalkingModule extends Module {
     priority = 75
     
     async onMessage(ctx, next) {
-        
         if (ctx.chat.id < 0) {
             const chat = await ctx.getInfo();
             
-            if (ctx.text === '/talk') {
-                if (!await isAdmin(ctx)) return;
-                
-                if (chat.talk) {
-                    delete chat['talk'];
-                    return await ctx.reply(`❌ <b>Режим общения</b> отключен!`)
-                }
-                
-                chat.talk = {
-                    enabled: true,
-                    memory: initialMemory(ctx)
-                }
-                
-                return await ctx.reply(`✅ <b>Режим общения</b> активирован!`)
-            }
+            if (ctx.text === '/talk') return await handleSwitchCommand(ctx, chat)
             
-            if (chat.talk) {
-                if (chat.talk.memory.length >= 20) chat.talk.memory.splice(1, 1)
+            if (chat.talk?.enabled) {
+                if (chat.talk.messages.length >= 8) chat.talk.messages.splice(0, 1)
                 
                 const msgContent = getContent(ctx);
                 const name = ctx.from.first_name + (ctx.from.last_name ? (" " + ctx.from.last_name) : "");
                 const from = name.length ? name : "безымянный"
                 const content = `${from}: ${msgContent}`
-                chat.talk.memory.push({ role: 'user', content })
+                chat.talk.messages.push({ role: 'user', content })
                 
                 const text = ctx.text;
                 if (text) {
-                    if (text.match(/ плут /i) || text.match(/^плут(.*)/i)) {
+                    if (regex().test(text)) {
                         if (!generating) {
                             const message = await getResponse(ctx, chat.talk)
                             if (message && message.content.length) {
                                 if (chat.talk) {
-                                    if (chat.talk.memory.length >= 20) chat.talk.memory.splice(1, 1)
-                                    chat.talk.memory.push(message)
+                                    if (chat.talk.messages.length >= 8) chat.talk.messages.splice(0, 1)
+                                    chat.talk.messages.push({ role: 'system', content: message.content })
                                 }
                                 await ctx.reply(message.content);
                             }
@@ -53,31 +38,65 @@ class AITalkingModule extends Module {
                     }
                 }
             }
-            
+        }
+        
+        if (ctx.text) {
+            if (ctx.text.match(/\/ai-talker(.*)/)) return await handleTalkerCommand(ctx)
         }
         
         next()
     }
 }
 
-const initialMemory = (ctx) => {
-    return [{
-      "role": "system",
-      "content": `ты бот по имени плут, хитрая лиса в публичном чате "${ctx.chat.title}".  
-говори всегда с маленькой буквы. иногда используй пунктуацию.  
-никогда не используй форматирование, смайлики или эмодзи.  
-отвечай максимум 1–3 предложениями, каждое до 10 слов.  
-отвечай только от себя, не копируй чужие реплики.  
-участники пишут так: "имя: текст". имя всегда до первого двоеточия.  
-отвечай на языке, на котором задают вопрос.  
-никогда не нарушай образ плута-лисы.`
-    }]
+let currentRegex;
+const regex = () => {
+    console.log(bot.me)
+    if (!currentRegex) currentRegex = new RegExp(`@${bot.me}\\b|(^|\\s)плут(\\s|$)`, 'i');
+    return currentRegex;
+}
+
+const handleSwitchCommand = async (ctx, chat) => {
+    if (!await isAdmin(ctx)) return;
+    
+    if (!chat.talk) chat.talk = { enabled: false };
+    
+    if (chat.talk.enabled) {
+        delete chat['talk'];
+        return await ctx.reply(`❌ <b>Режим общения</b> отключен!`)
+    }
+    
+    const user = await ctx.getUser();
+    
+    if (!user.talk?.system) {
+        return await ctx.reply(`❌ Задайте системное сообщение в личке бота /ai-talker system (текст)`)
+    }
+    
+    chat.talk.enabled = true;
+    chat.talk.messages = [];
+    chat.talk.system = user.talk.system;
+    
+    return await ctx.reply(`✅ <b>Режим общения</b> активирован!`)
+}
+
+const handleTalkerCommand = async (ctx) => {
+    if (ctx.chat.id < 0) return await ctx.reply(`Команда доступна в личке бота!`)
+    const user = await ctx.getUser();
+    
+    if (!user.talk) user.talk = {};
+    
+    if (ctx.text.match(/^\/ai-talker system (.*)/)) {
+        const system = ctx.text.split(' ').slice(2).join(' ');
+        if (system.length < 20 || system.length > 1000) return await ctx.reply(`Длина от 20 до 1000 символов, вы ввели ${system.length} симв.`)
+        user.talk.system = system;
+        await ctx.reply(`Успешно!`);
+    }
 }
 
 let generating = false;
 
 const getContent = ctx => {
-    if (ctx.text) return ctx.text.length > 250 ? ctx.text.slice(0, 250) : ctx.text;
+    console.log(Object.keys(ctx.update))
+    if (ctx.text) return ctx.text.length > 200 ? ctx.text.slice(0, 200) : ctx.text;
     else if (ctx.sticker) return "[стикер, который ты не сможешь увидеть]"
     else if (ctx.video) return "[видео, которое ты не сможешь посмотреть]"
     else if (ctx.photo) return "[фото, которое ты не сможешь увидеть]"
@@ -89,7 +108,18 @@ const getContent = ctx => {
 
 const getResponse = async (ctx, entry) => {
     generating = true;
-    console.log(JSON.stringify(entry.memory))
+    
+    const messages = [{
+            role: 'system',
+            content: entry.system
+        },
+        {
+            role: 'system',
+            content: 'Никогда не пиши более 2 предложений'
+        },
+        ...entry.messages,
+    ];
+    console.log(messages)
     
     const resp = await fetch("http://localhost:11434/api/chat", {
         method: "POST",
@@ -97,7 +127,7 @@ const getResponse = async (ctx, entry) => {
         body: 
             JSON.stringify({
                 model: "gemma3:12b",
-                messages: entry.memory,
+                messages,
                 num_predict: 64,
                 stream: false,
             })
